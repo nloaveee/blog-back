01a1d8a1f1a01081eebfad07ac4ac8ebd36b73c3
hyeonheee <nloavee@gmail.com>
2024-10-30 18:27:48 +0900

    파일 업로드, 불러오기 API

---
+++ src/main/java/study/blogback/service/FileService.java
@@ -0,0 +1,10 @@
+package study.blogback.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileService {
+
+    String upload (MultipartFile file);
+    Resource getImage (String filename);
+}
---
+++ src/main/java/study/blogback/service/implement/FileServiceImplement.java
@@ -0,0 +1,57 @@
+package study.blogback.service.implement;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import study.blogback.service.FileService;
+
+import java.io.File;
+import java.util.UUID;
+
+@Service
+public class FileServiceImplement implements FileService {
+
+    @Value("${file.path}")
+    private String filePath;
+    @Value("${file.url}")
+    private String fileUrl;
+
+    @Override
+    public String upload(MultipartFile file) {
+
+        if (file.isEmpty()) return null;
+
+        String originalFilename = file.getOriginalFilename();
+        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String uuid = UUID.randomUUID().toString();
+        String saveFileName = uuid + extension;
+        String savePath = filePath + saveFileName;
+
+        try {
+            // 파일 저장
+            file.transferTo(new File(savePath));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+
+        String url = fileUrl + saveFileName;
+        return url;
+    }
+
+    @Override
+    public Resource getImage(String filename) {
+        Resource resource = null;
+
+        try {
+            resource = new UrlResource("file:"+filePath+filename);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+
+        return resource;
+    }
+}
